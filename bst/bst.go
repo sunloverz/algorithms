e0677b2c0fdc630e14f794bac13358e9bb3524fe
sunloverz <sunloverz.kg@gmail.com>
2020-01-31 17:39:17 +0600

    Implement binary search tree

---
+++ bst/bst.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+type BinarySearchTree struct {
+	Root *Node
+}
+
+func (t *BinarySearchTree) Insert(value int) {
+	t.Root = insertNode(t.Root, value)
+}
+
+func (t *BinarySearchTree) Search(value int) bool {
+	return search(t.Root, value)
+}
+
+func search(root *Node, value int) bool {
+	if root == nil {
+		return false
+	} else if root.Value == value {
+		return true
+	} else if value > root.Value {
+		return search(root.Right, value)
+	} else {
+		return search(root.Left, value)
+	}
+}
+
+func insertNode(root *Node, value int) *Node {
+	if root == nil {
+		root = &Node{Value: value}
+	} else if value > root.Value {
+		root.Right = insertNode(root.Right, value)
+	} else {
+		root.Left = insertNode(root.Left, value)
+	}
+	return root
+}
+
+func main() {
+	var tree BinarySearchTree
+	tree.Insert(15)
+	tree.Insert(10)
+	tree.Insert(20)
+	tree.Insert(25)
+	tree.Insert(6)
+	tree.Insert(12)
+	fmt.Println(tree.Root.Value)
+	fmt.Println(tree.Search(6))
+	fmt.Println(tree.Search(10))
+	fmt.Println(tree.Search(25))
+    fmt.Println(tree.Search(30))
+}
